42ef23bc4c8b5c97e985de7f66d4d7527bc36671
fargelus <ddraudred@gmail.com>
2023-05-13 17:17:43 +0400

    RecordBtn component

--- drum-kit/src/components/DrumsKit.vue
+++ drum-kit/src/components/DrumsKit.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="drums-kit">
-    <drum-key
-      v-for="sound in sounds"
-      @sound="onSound"
-      :is-active="!!isCodeActive(sound.code)"
-      :key="sound.code"
-      :code="sound.code"
-      :type="sound.type"
-    >
-    </drum-key>
+    <div class="drums-kit__keys">
+      <drum-key
+        v-for="sound in sounds"
+        @sound="onSound"
+        :is-active="!!isCodeActive(sound.code)"
+        :key="sound.code"
+        :code="sound.code"
+        :type="sound.type"
+      >
+      </drum-key>
+    </div>
+    <div class="drums-kit__controls">
+      <record-btn @record="onRecord" @pause="onPause"></record-btn>
+    </div>
   </div>
 </template>
 
 <script>
 import DrumKey from "./DrumKey.vue";
+import RecordBtn from "./RecordBtn.vue";
 
 export default {
   name: "DrumsKit",
-  components: { DrumKey },
+  components: { DrumKey, RecordBtn },
   data() {
     return {
       pressedKey: null,
+      recordKeys: [],
+      recording: false,
       sounds: [
         {
           code: "A",
@@ -70,10 +78,18 @@ export default {
     setPressedKey(ev) {
       const key = ev.key.toUpperCase();
       this.pressedKey = this.isKeyWatched(key) ? key : null;
+      this.checkRecording();
     },
 
     isKeyWatched(key) {
       return this.sounds.find((sound) => sound.code === key);
+    },
+
+    checkRecording() {
+      if (this.recording && this.pressedKey) {
+        this.recordKeys.push(this.pressedKey);
+        this.pressedKey = null;
+      }
     },
 
     isCodeActive(code) {
@@ -83,6 +99,15 @@ export default {
     onSound(duration) {
       setTimeout(() => (this.pressedKey = null), duration * 1000);
     },
+
+    onRecord() {
+      this.recording = true;
+      this.recordKeys = [];
+    },
+
+    onPause() {
+      this.recording = false;
+    },
   },
 };
 </script>
@@ -90,8 +115,17 @@ export default {
 <style scoped>
 .drums-kit {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
+}
+
+.drums-kit__keys {
+  display: flex;
+}
+
+.drums-kit__controls {
+  margin-top: 20px;
 }
 </style>
---
+++ drum-kit/src/components/RecordBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="toggleRecord" class="record-btn">{{ icon }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecordBtn",
+  data() {
+    return {
+      isRecordStarted: false,
+    };
+  },
+
+  methods: {
+    toggleRecord() {
+      this.isRecordStarted = !this.isRecordStarted;
+      this.isRecordStarted ? this.$emit("record") : this.$emit("pause");
+    },
+  },
+
+  computed: {
+    icon() {
+      return this.isRecordStarted ? "⏸️" : "⏺️";
+    },
+  },
+};
+</script>
+
+<style>
+.record-btn {
+  cursor: pointer;
+  background: transparent;
+  border: 0;
+  font-size: 30px;
+  padding: 0;
+}
+</style>
